676f423cf0bff9c9887c75c68d16a53dd57344bc
cth <tianhao.cuit@item.com>
2024-12-06 16:05:51 +0800

    异常处理默认输出个日志

--- src/main/java/com/logHelper/handler/DefaultOnExceptionHandler.java
+++ src/main/java/com/logHelper/handler/DefaultOnExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.logHelper.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 
 /**
@@ -7,10 +8,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
  * Author: cth
  * Created Date: 2024-12-05
  */
+@Slf4j
 public class DefaultOnExceptionHandler extends OnExceptionHandler{
 
     @Override
     public void onException(ProceedingJoinPoint point, Exception e) {
+        log.error(e.getMessage());
         return;
     }
 }
